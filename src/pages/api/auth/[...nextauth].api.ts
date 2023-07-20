fa18742f00381946192dbf99bea3b45a8f97ea4f
Pablo Alan <pabloxt14@gmail.com>
2023-07-20 19:54:40 -0300

    feat: configured calendar access permission

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,8 +6,26 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+  callbacks: {
+    // Ações de callback que executa algo após o usuário logar/autenticar na aplicação
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
