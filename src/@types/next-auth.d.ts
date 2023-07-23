fc3ff46e478c13d7cd21ef34ecfefe140bd45a6a
Pablo Alan <pabloxt14@gmail.com>
2023-07-23 16:15:28 -0300

    feat: started create PrismaAdapter for NextAuth

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,12 @@
+// Essa tipagem serve para o NextAuth saber como é a estrutura das nossas tabelas criadas no banco de dados (as quais realizamos alterações que diferem da estrutura padrão do NextAuth em relação a estrutura de autenticação).
+import NextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  export interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
+}
